afc6672ba66d10008cb663b552349674569ad8cb
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-09-08 19:44:51 +0800

    Styling the popup window

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const cardProjects = [
 ];
 
 const btn = document.querySelectorAll('.btn-card');
-const popupContainer = document.querySelector('.popup-container');
+const c = document.querySelector('.popup-container');
 const main = document.querySelector('main');
 
 function displayAll(id) {
@@ -54,23 +54,52 @@ function displayAll(id) {
   main.style.display = 'none';
   const card = cardProjects[id];
   popupContainer.innerHTML = `<div class="overlay"></div>
-<div class="content">
-  <h2>${card.Title}</h2>
-  <img src="./images/snapshot4.svg" alt="">
-  <p>
-   ${card.Description}
-  </p>
-  <ul class="pop-list">
-    <li>${card.Tech[0]}</li>
-    <li>${card.Tech[1]}</li>
-    <li>${card.Tech[2]}</li>
-  </ul>
-  <div class="btnContainer">
-    <button type="button" class="btn-see-popup" id="live">See Live</button>
-    <button type="button" class="btn-see-popup" id="sources">See Sources</button>
+  <div class="content">
+  <div class="titles-section">
+  <h2>Multi-Post Stories</h2>
+  <i class="bi bi-x-lg"></i></div>
+  <div class="list-class">
+    <ul>
+      <li>Canopy</li>
+      <li class="circle-iron"></li>
+      <li>Back end Dev</li>
+      <li class="circle-iron"></li>
+      <li>2015</li>
+    </ul>
   </div>
+
+  <div class="image-class-section">
+    <img class="mobile" src="./images/snapshot4.svg">
+    <img class="desktop" src="./images/sampleDesktopImage.png">
+  </div>
+  <div  class="container-description">
+    <div class="text-paragraph"><p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+    </p></div>
+    <div class="button-container-popup">
+      <ul class="pop-list">
+        <li>html</li>
+        <li>bootstrap</li>
+        <li>ruby</li>
+      </ul>
+      <ul class="link-action pop-list">
+        <li >Javascript</li>
+        <li >css</li>
+        <li >github</li>
+
+      </ul>
+      <div class="btnContainer">
+        <button type="button" class="btn-see-popup" id="live">See Live<img src="./images/Icon.svg"></button>
+        <button type="button" class="btn-see-popup" id="sources">See Sources<img src="./images/Vector.svg"></button>
+      </div>
+    </div>
+    
+  </div>
+  
+  
 </div>
 `;
+
 }
 
 btn.forEach((eachbtn) => {
