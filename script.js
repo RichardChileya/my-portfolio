6ee92be98dfd81ce11cd817388fcc382c6d741c8
RichardChileya <richardchileyawk@gmail.com>
2022-09-09 09:23:21 +0200

    add javascript for popup window

--- script.js
+++ script.js
@@ -20,73 +20,59 @@ list.forEach((lists) => {
 
 const cardProjects = [
   {
-    Image: './images/snapshot4.svg',
-    Title: 'Multi-Post Stories',
-    Description: 'A daily selection of privately personalized reads; no accounts or sign-up',
-    Tech: ['html', 'css', 'jaavascript'],
+    Image: './images/snapshot.png',
+    Image2: './images/sampleDesktopImage.png',
+    header: 'Tonic',
+    Description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    Tech1: ['html', 'css', 'jaavascript'],
+    Tech2: ['github', 'bootstrap', 'ruby'],
+    Canopy: ['Canopy', 'Back-end', '2015'],
   },
-  {
-    Image: './images/snapshot4.svg',
-    Title: 'Multi-Post Stories',
-    Description: 'A daily selection of privately personalized reads; no accounts or sign-up',
-    Tech: ['html', 'css', 'jaavascript'],
-  },
-  {
-    Image: './images/snapshot4.svg',
-    Title: 'Multi-Post Stories',
-    Description: 'A daily selection of privately personalized reads; no accounts or sign-up',
-    Tech: ['html', 'css', 'jaavascript'],
-  },
-  {
-    Image: './images/snapshot4.svg',
-    Title: 'Multi-Post Stories',
-    Description: 'A daily selection of privately personalized reads; no accounts or sign-up',
-    Tech: ['html', 'css', 'jaavascript'],
-  },
+
 ];
 
 const btn = document.querySelectorAll('.btn-card');
-const c = document.querySelector('.popup-container');
+const popupContainer = document.querySelector('.popup-container');
 const main = document.querySelector('main');
 
 function displayAll(id) {
   popupContainer.style.display = 'flex';
   main.style.display = 'none';
   const card = cardProjects[id];
-  popupContainer.innerHTML = `<div class="overlay"></div>
+  popupContainer.innerHTML = `
   <div class="content">
+  <div class="close-btn">&times;</div>
   <div class="titles-section">
-  <h2>Multi-Post Stories</h2>
-  <i class="bi bi-x-lg"></i></div>
+   <h2> Tonic</h2>
+    
+  </div>
   <div class="list-class">
     <ul>
-      <li>Canopy</li>
+      <li>${card.Canopy[0]}</li>
       <li class="circle-iron"></li>
-      <li>Back end Dev</li>
+      <li>${card.Canopy[1]}</li>
       <li class="circle-iron"></li>
-      <li>2015</li>
+      <li>${card.Canopy[2]}</li>
     </ul>
   </div>
 
   <div class="image-class-section">
-    <img class="mobile" src="./images/snapshot4.svg">
-    <img class="desktop" src="./images/sampleDesktopImage.png">
+    <img class="mobile" src="${card.Image}">
+    <img class="desktop" src="${card.Image2}">
+    
   </div>
   <div  class="container-description">
-    <div class="text-paragraph"><p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
-    </p></div>
+    <div class="text-paragraph"><p> ${card.Description}</p></div>
     <div class="button-container-popup">
-      <ul class="pop-list">
-        <li>html</li>
-        <li>bootstrap</li>
-        <li>ruby</li>
+      <ul class="pop-list desktop">
+        <li>${card.Tech2[0]}</li>
+        <li>${card.Tech2[2]}</li>
+        <li>${card.Tech2[1]}</li>
       </ul>
-      <ul class="link-action pop-list">
-        <li >Javascript</li>
-        <li >css</li>
-        <li >github</li>
-
+      <ul class="link-action pop-list mobile">
+      <li>${card.Tech1[0]}</li>
+      <li>${card.Tech1[1]}</li>
+      <li>${card.Tech1[2]}</li>
       </ul>
       <div class="btnContainer">
         <button type="button" class="btn-see-popup" id="live">See Live<img src="./images/Icon.svg"></button>
@@ -95,15 +81,17 @@ function displayAll(id) {
     </div>
     
   </div>
-  
-  
 </div>
 `;
-
 }
+function togglePopup(){
+  document.querySelector('.close-btn').classList.toggle('active');
+}
+
 
 btn.forEach((eachbtn) => {
   eachbtn.addEventListener('click', (e) => {
     displayAll(e.target.id);
   });
+
 });
