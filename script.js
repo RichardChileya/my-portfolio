5e8e46bc3ee0a947a62c20490cb00e2e61fbf402
RichardChileya <richardchileyawk@gmail.com>
2023-03-25 23:25:07 +0200

    Add projects to my portfolio

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ const width = window.innerWidth;
 const cards = [
   {
     key: 0,
-    name: 'Tonic',
+    name: 'Geolex Digital Events',
     option: ['CANOPY', 'Back End Dev', '2015'],
     optionMobile: ['CANOPY', 'Back End Dev', '2015'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    description: 'Geolex Digital Space is an online events website designed to showcase digital events happening across the globe. As a Capstone project, the website is built using HTML, CSS, and JavaScript. It offers users a comprehensive and user-friendly platform to explore various digital events, including webinars, conferences, and virtual workshops The website iss design is modern and visually appealing, making it easy to navigate and browse through different events. The website is responsiveness ensures that users can access it on any device, including smartphones and tablets.',
     featuredImage: './images/photo1dt.png',
     desktopImage: 'images/photo1dt.png',
     technologies: ['html', 'css', 'javascript', 'ruby'],
@@ -24,39 +24,39 @@ const cards = [
   },
   {
     key: 1,
-    name: 'Multi-post Stories',
-    option: ['FACEBOOK', 'Back End Dev', '2015'],
+    name: 'Around the World',
+    option: ['React Capstone', 'FrontEnd ', 'Oct-2022'],
     optionMobile: ['CANOPY', 'Back End Dev', '2015'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    featuredImage: './images/photo2dt.png',
-    desktopImage: './images/photo2dt.png',
+    description: 'Around the World is a React application that showcases various core features of the React framework. The project displays information about different countries around the world fetched from an API. It implements React features such as components, props, states, and API requests, as well as loggers for troubleshooting. The application is a valuable tool for enhancing React development skills while also exploring the worlds various countries and cultures.',
+    featuredImage: './images/Around the world.png',
+    desktopImage: './images/Around the world.png',
     technologies: ['html', 'css', 'javascript', 'ruby'],
-    linktoliveversion: 'https://richardchileya.github.io/portfolioskeleton1/',
+    linktoliveversion: 'https://expolore-countries.netlify.app/',
     linktosource: 'https://github.com/RichardChileya/portfolioskeleton1',
   },
   {
     key: 2,
-    name: 'FACEBOOK 360',
-    option: ['FACEBOOK', 'Full Stack Dev', '2015'],
-    optionMobile: ['CANOPY', 'Back End Dev', '2015'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ',
-    featuredImage: './images/photo3dt.png',
-    desktopImage: './images/photo3dt.png',
+    name: 'Awesome Books Store',
+    option: ['MicroV', 'Full Stack Dev', 'Sept-2022'],
+    optionMobile: ['Geolex', 'Frontend Dev', '2022'],
+    description: 'The Bookstore web app is a React-based platform that enables users to explore a collection of books, add their favorite authors to a library, and leave reviews. It fetches data from an API and uses Redux middleware for asynchronous communication, providing users with an exceptional experience. With a user-friendly interface and features such as book discovery and discussion. Excellent resource for book enthusiasts who wish to expand their literary horizons and track their  progress.',
+    featuredImage: './images/Awesome Books.png',
+    desktopImage: './images/Awesome Books.png',
     technologies: ['html', 'css', 'javascript', 'ruby'],
-    linktoliveversion: 'https://richardchileya.github.io/portfolioskeleton1//',
-    linktosource: 'https://github.com/RichardChileya/portfolioskeleton1',
+    linktoliveversion: 'https://awesome-books-rico.netlify.app/',
+    linktosource: 'https://github.com/RichardChileya/book-store.git',
   },
   {
     key: 3,
-    name: 'Uber Navigation',
-    option: ['Uber', 'Lead Developer', '2018'],
+    name: 'Space Travellers Hub', // modal popup
+    option: ['NASA ', 'Lead Developer', '2022'],
     optionMobile: ['CANOPY', 'Back End Dev', '2015'],
     description: ' Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    featuredImage: './images/photo4dt.png',
-    desktopImage: './images/photo4dt.png',
+    featuredImage: './images/SpaceHub.png',
+    desktopImage: './images/SpaceHub.png',
     technologies: ['html', 'css', 'javascript', 'git hub', 'ruby', 'Bootstrap'],
-    linktoliveversion: 'https://richardchileya.github.io/portfolioskeleton1//',
-    linktosource: 'https://github.com/RichardChileya/portfolioskeleton1',
+    linktoliveversion: 'https://space-travellers-hub-rwk.netlify.app/',
+    linktosource: 'https://github.com/RichardChileya/space-traveler-s-hub.git',
   },
 ];
 
@@ -125,7 +125,7 @@ function showWorks() {
       options: ['Capstone Project 1', 'Full-stack', '2022'],
       imgMobile: 'images/first-capstone.png',
       imgDesktop: 'images/first-capstone.png',
-      description: 'A Project about a Digital Safety Event .',
+      description: 'Geolex Digital Space is a project showcasing an online events website that highlights digital events happening worldwide. Built using HTML, CSS, and JavaScript, the website provides a comprehensive and user-friendly platform for users to explore webinars, conferences, virtual workshops, and more. Its modern and visually appealing design allows for easy navigation.',
       descriptionD: 'A Project about Cyber Security and Digital Safety.',
       languages: ` <li class="langu langu-a">html</li>
       <li class="langu langu-b">css</li>
@@ -135,11 +135,11 @@ function showWorks() {
       <li class="langu2">javascript</li>`,
     },
     {
-      title: 'Multi-post Stories',
-      options: ['FACEBOOK', 'Back End Dev', '2015'],
+      title: 'Around the World',
+      options: ['Capstone Project', 'Frontend Dev', '2022'],
       imgMobile: 'images/photo2.png',
-      imgDesktop: 'images/photo2dt.png',
-      description: 'Experimental content creation features that allows users to add and existing story over the course of the day without spamming their friends.',
+      imgDesktop: 'images/Around the world.png',
+      description: 'Around the word, A React application : Implementing React Core features varying from components, Props, States, API, To Loggers, the project displays content fetched from an API for world information on countries and more.',
       descriptionD: 'A daily selection of privately personalized reads; no accounts or sign-ups required. ',
       languages: `<li class="langu">html</li>
       <li class="langu">css</li>
@@ -150,11 +150,11 @@ function showWorks() {
       <li class="langu2">javascript</li>`,
     },
     {
-      title: 'Facebook 360',
-      options: ['FACEBOOK', 'Full Stack Dev', '2015'],
-      imgMobile: 'images/photo3.png',
-      imgDesktop: 'images/photo3dt.png',
-      description: "Exploring the future of media in Facebook's first Virtual Reality place to discover and enjoy 360 photos and videos on Gear VR .",
+      title: 'Awesome Books ',
+      options: ['MicroV Library', 'Full Stack Dev', 'Sept-2022'],
+      imgMobile: 'images/Awesome Books.png',
+      imgDesktop: 'images/Awesome Books.png',
+      description: "The Bookstore web app is a React-based application that allows users to search for books, add books from their favorite authors to a personal library, leave reviews and ratings, and discover new books. The app makes use of Redux middleware for asynchronous communication between the app and the API.",
       descriptionD: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       languages: `<li class="langu">html</li>
       <li class="langu">css</li>
@@ -165,17 +165,17 @@ function showWorks() {
       <li class="langu2">javascript</li>`,
     },
     {
-      title: 'Uber Navigation',
+      title: 'Space Travellers Rico',
       options: ['Uber', 'Lead Developer', '2018'],
-      imgMobile: 'images/photo4.png',
-      imgDesktop: 'images/photo4dt.png',
-      description: 'A smart assistant to make driving mode safe, efficient, and fun by unlocking your most expensive computer: your car.',
+      imgMobile: './images/SpaceHub.png',
+      imgDesktop: './images/SpaceHub.png',
+      description: 'A website that uses a real live api from space x and displays it, You are able to Reserve a rocket and you will have it on your profile page aslo join missions which you will be able to see it on your profile..',
       descriptionD: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       languages: `<li class="langu">html</li>
       <li class="langu">css</li>
       <li class="langu">javascript</li>
       <li class="langu2">html</li>
-      <li class="langu2">Ruby on rails</li>
+      <li class="langu2">DOM</li>
       <li class="langu2">css</li>
       <li class="langu2">javascript</li>`,
     },
